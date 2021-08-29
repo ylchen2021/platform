e1c48499f25da5f28fb5d1f320fba63d1a3e923b
chenyilin <yilin.chen@faw-vw.com>
2021-08-29 11:12:21 +0800

    BaseDialog & loading动画

---
+++ src/main/java/com/boostvision/platform/ui/BaseDialog.kt
@@ -0,0 +1,55 @@
+package com.boostvision.platform.ui
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.*
+import androidx.fragment.app.DialogFragment
+import com.boostvision.platform.utils.UIUtils
+
+abstract class BaseDialog protected constructor() : DialogFragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        dialog?.setCanceledOnTouchOutside(false)
+        dialog?.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        val window = dialog?.window
+        if (window != null) {
+            window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            val lp = window.attributes
+            lp.width = getLayoutWidth()
+            lp.height = getLayoutHeight()
+            lp.gravity = getGravity()
+            window.decorView.setPadding(0, 0, 0, 0)
+            window.attributes = lp
+        }
+        return inflater.inflate(getLayoutRes(), container, false)
+    }
+
+    protected open fun getGravity(): Int {
+        return Gravity.CENTER
+    }
+
+    protected open fun getLayoutWidth(): Int {
+        return ViewGroup.LayoutParams.MATCH_PARENT
+    }
+
+    protected open fun getLayoutHeight(): Int {
+        return ViewGroup.LayoutParams.WRAP_CONTENT
+    }
+
+    protected abstract fun getLayoutRes(): Int
+
+    protected open fun getWidthPaddingDp(): Int {
+        return 15
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val dialog = dialog
+        if (dialog != null) {
+            val dm = DisplayMetrics()
+            requireActivity().windowManager.defaultDisplay.getMetrics(dm)
+            dialog.window?.setLayout(UIUtils.getScreenWidth(context) - UIUtils.dp2px(context, getWidthPaddingDp()), getLayoutHeight())
+        }
+    }
+}
