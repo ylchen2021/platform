ec72e544075b456b78353bbbe82ae2a5851eb2b5
chenyilin <yilin.chen@faw-vw.com>
2021-12-21 14:38:36 +0800

    remote config 接入

---
+++ src/main/java/remote/common/config/ConfigManager.kt
@@ -0,0 +1,37 @@
+package remote.common.config
+
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import tv.remote.platform.BuildConfig
+
+object ConfigManager {
+    private lateinit var remoteConfig: FirebaseRemoteConfig
+
+    fun init(defaultValueRes: Int) {
+        remoteConfig = Firebase.remoteConfig
+        if (BuildConfig.DEBUG) {
+            val configSettings = remoteConfigSettings {
+                minimumFetchIntervalInSeconds = 0
+            }
+            remoteConfig.setConfigSettingsAsync(configSettings)
+        }
+        if (defaultValueRes > 0) {
+            remoteConfig.setDefaultsAsync(defaultValueRes)
+        }
+        remoteConfig.fetchAndActivate()
+    }
+
+    fun getBoolean(key: String): Boolean {
+        return remoteConfig.getBoolean(key)
+    }
+
+    fun getLong(key: String): Long {
+        return remoteConfig.getLong(key)
+    }
+
+    fun getString(key: String): String {
+        return remoteConfig.getString(key)
+    }
+}
