edc3506e4c5f13462b45a1ef90ed149b842dc05f
chenyilin <yilin.chen@faw-vw.com>
2021-12-23 11:43:50 +0800

    添加activity返回值支持

--- src/main/java/remote/common/ui/BaseActivity.kt
+++ src/main/java/remote/common/ui/BaseActivity.kt
@@ -1,5 +1,6 @@
 package remote.common.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
@@ -7,8 +8,19 @@ import androidx.appcompat.app.AppCompatActivity
 abstract class BaseActivity: AppCompatActivity() {
     protected abstract fun getContentRes(): Int
 
+    private var resultHandler: ((Int, Int, Intent?)->Unit)? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(getContentRes())
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        resultHandler?.invoke(requestCode, resultCode, data)
+    }
+
+    fun setOnActivityResultHandler(handler: (Int, Int, Intent?)->Unit) {
+        resultHandler = handler
     }
 }
